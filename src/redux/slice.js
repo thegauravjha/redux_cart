fcb0dc870046270d53641906650ae2093cda4bcd
thegauravjha <gaurav.jhaji20@gmail.com>
2024-04-13 19:15:04 +0530

    changed total of initial value

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
-        total: 11,
+        total: 8,
         items: [
             {
                 key: 1,
